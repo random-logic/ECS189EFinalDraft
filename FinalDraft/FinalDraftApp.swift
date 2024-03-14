5f98248e38e504dcd5ad19ff9d61df3407fb5ebb
Andrew <andrewjowe@yahoo.com>
2024-03-13 17:42:40 -0700

    init

---
+++ FinalDraft/FinalDraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalDraftApp.swift
+//  FinalDraft
+//
+//  Created by Andrew Jowe on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalDraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
